948b07b584a03c559aa223ee3b65219f35888585
Knaoinr <knaoinr@gmail.com>
2019-11-18 17:39:43 -0500

    Updated robot.java for the young ones

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,17 @@
 package frc.robot;
 
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.cscore.UsbCamera;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+// import edu.wpi.first.cameraserver.CameraServer;
+// import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.*;
-import frc.robot.commands.autonomous.*;
-import frc.robot.subsystems.Subsystems;
-import frc.robot.userinterface.UserInterface;
+// import edu.wpi.first.wpilibj.command.CommandGroup;
+// import edu.wpi.first.wpilibj.command.Scheduler;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import frc.robot.commands.*;
+// import frc.robot.commands.autonomous.*;
+// import frc.robot.subsystems.Subsystems;
+// import frc.robot.userinterface.UserInterface;
 
 public class Robot extends TimedRobot {
 
@@ -33,9 +33,9 @@ public class Robot extends TimedRobot {
         - Once you've made subsystems:
             1. Start the compressor
             2. Set any initial positions (here, set guillotine to lowest position)
-            3. We have fancy LEDs, so use the command Subsystems.arduino.sendCommand("0001111");
+            3. We have fancy LEDs, so use the command -> Subsystems.arduino.sendCommand("0001111");
         - Once you've made the user interface & commands for them:
-            1. Connect buttons with commands, e.g. (insert button).whenPressed(new IntakeGrab());
+            1. Connect buttons with commands, e.g. -> (insert button).whenPressed(new IntakeGrab());
             Note that this only used for commands that should be executed on a button press.
             Here's what the buttons should do in total:
 
@@ -55,84 +55,59 @@ public class Robot extends TimedRobot {
 
     }
 
+    /*
+    Now, you'll have 6 methods: combinations of disabled/autonomous/teleop and init/periodic.
+    Follow these guidelines for each:
+    - All inits remove all commands in the scheduler
+    - If we have LEDs (and we do here!) change color in all inits
+    - Auto and teleop periodics run commands in the scheduler
+    - All periodics print data to SmartDashboard (see bottom of file)
+    - Disabled stops robot function to some extent
+    - Auto init includes auto commands
+    - Teleop periodic includes motor speed control
+    */
+
     public void disabledInit() {
-        Subsystems.arduino.sendCommand("0001111");
+        // Subsystems.arduino.sendCommand("0001111");
     }
 
     public void autonomousInit() {
-        Scheduler.getInstance().removeAll();
-        Subsystems.arduino.sendCommand("0005551");
-        String gameData = DriverStation.getInstance().getGameSpecificMessage();
-        if (UserInterface.launchpad.getMultiSwitchLeft()) {
-            autonomous = new LeftAutonomous(gameData, UserInterface.launchpad.getSwitch1());
-        } else if (UserInterface.launchpad.getMultiSwitchInactive()) {
-            autonomous = new CenterAutonomous(gameData.charAt(0));
-        } else if (UserInterface.launchpad.getMultiSwitchRight()) {
-            autonomous = new RightAutonomous(gameData, UserInterface.launchpad.getSwitch1());
-        }
-        //autonomous.start();
-        rotations.start();
+        // Scheduler.getInstance().removeAll();
+        // Subsystems.arduino.sendCommand("0005551");
+        // Start any autonomous command group with -> (insert name).start();
     }
 
     public void teleopInit() {
-        if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Red) {
-            Subsystems.arduino.sendCommand("0003331");
-        } else {
-            Subsystems.arduino.sendCommand("0002221");
-        }
-        Scheduler.getInstance().removeAll();
+        // Scheduler.getInstance().removeAll();
+        // You can use the following code to decide if the team's color is red or blue! ->
+        // if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Red) {
+        //     Subsystems.arduino.sendCommand("0003331");
+        // } else {
+        //     Subsystems.arduino.sendCommand("0002221");
+        // }
     }
 
     public void disabledPeriodic() {
-        printDataToSmartDashboard();
+        // ...
     }
     
     public void autonomousPeriodic() {
-        Scheduler.getInstance().run();
-        printDataToSmartDashboard();
+        // Scheduler.getInstance().run();
     }
 
     public void teleopPeriodic() {
-        Subsystems.guillotine.setLiftSpeed(0.0d);
-        if (UserInterface.operatorController.getLeftJoystickY() < -.1d) {
-            Subsystems.intake.release(); // if controller left joystick goes up, arms release
-            Subsystems.guillotine.setLiftSpeed(1.0d);
-        } else if (UserInterface.operatorController.getLeftJoystickY() > .1d) {
-            Subsystems.guillotine.setLiftSpeed(-0.4d);
-            if (Subsystems.guillotine.getLowerSwitchValue()) {
-                Subsystems.guillotine.kick(); // when going down with the left joystick, if all the way down, kick cube
-            }
-        }
-        Subsystems.intake.setArmsSpeed(0.0d);
-        if (UserInterface.operatorController.getLeftTrigger() > 0.1d) {
-            Subsystems.intake.setArmsSpeed(-0.9d);
-        } else if (UserInterface.operatorController.getRightTrigger() > 0.1d) {
-            Subsystems.intake.setArmsSpeed(0.5d);
-        }
-        Subsystems.intake.setPivotSpeed(0.0d);
-        if (UserInterface.operatorController.getRightJoystickY() < -0.1d) {
-            Subsystems.guillotine.kick(); // kicker open if intake pivot goes up
-            Subsystems.intake.setPivotSpeed(0.6);
-        } else if (UserInterface.operatorController.getRightJoystickY() > 0.1d) {
-            Subsystems.intake.setPivotSpeed(-0.2);
-        }
-        Scheduler.getInstance().run();
-        printDataToSmartDashboard();
+        // Scheduler.getInstance().run();
+        /*
+        Here you'll manage all the robot movements that aren't already covered in the button code in
+        robotInit() or contained in the drive base. Once you've made your subsystems and user interface,
+        find out which parts need to be moved!
+        */
     }
 
-    private void printDataToSmartDashboard() {
-        SmartDashboard.putNumber("Ultrasonic", Subsystems.intake.getUltrasonicDistance());
-        SmartDashboard.putBoolean("Lift Upper Switch", Subsystems.guillotine.getUpperSwitchValue());
-        SmartDashboard.putBoolean("Lift Lower Switch", Subsystems.guillotine.getLowerSwitchValue());
-        SmartDashboard.putBoolean("Intake Upper Switch", Subsystems.intake.getUpperSwitchValue());
-        SmartDashboard.putBoolean("Intake Lower Switch", Subsystems.intake.getLowerSwitchValue());
-        SmartDashboard.putNumber("Guillotine Position", Subsystems.guillotine.getLiftPosition());
-        SmartDashboard.putNumber("Left Encoder", Subsystems.driveBase.getLeftPosition());
-        SmartDashboard.putNumber("Right Encoder", Subsystems.driveBase.getRightPosition());
-        SmartDashboard.putNumber("Left Arm Current", Subsystems.intake.getLeftArmCurrent());
-        SmartDashboard.putNumber("Right Arm Current", Subsystems.intake.getRightArmCurrent());
-        SmartDashboard.putNumber("Xbox POV", UserInterface.operatorController.getPOVAngle());
-        SmartDashboard.putNumber("Gyro Angle", Subsystems.driveBase.getGyroAngle());
+    /*
+    Here, you'll want to write a private void method that prints the values of variables to the Smart Dashboard.
+    To do this, the general command is -> SmartDashboard.putNumber("Name", Something.getValue());
+    However, there are also commands like putBoolean rather than putNumber that you can use.
+    */
 
-    }
 }
