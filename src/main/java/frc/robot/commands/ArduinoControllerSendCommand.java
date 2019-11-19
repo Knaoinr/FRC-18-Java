35a07fbbb3847cb7f8919a774a4f6cca9ea3a6a8
Knaoinr <knaoinr@gmail.com>
2019-11-18 21:28:21 -0500

    Finished updating for now ^-^

--- src/main/java/frc/robot/commands/ArduinoControllerSendCommand.java
+++ src/main/java/frc/robot/commands/ArduinoControllerSendCommand.java
@@ -1,10 +1,25 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.DriverStation;
+// import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.subsystems.Subsystems;
+// import frc.robot.subsystems.Subsystems;
 
 public class ArduinoControllerSendCommand extends Command {
+
+    /*
+    Here's a simpler example of a command!
+    
+    Commands have six parts:
+    1. Constructor - super(String name), requires whichever subsystem
+    2. initialize() - sets timeout if not done in constructor, zeroes instruments
+    3. execute() - contains things to be done
+    4. isFinished() - returns whether the command should end or not
+    5. interrupted() - executed when the command is manually stopped
+    6. end() - called when isFinished() returns true
+
+    Try and see what they do here and in other classes, then try making some of your own!
+
+    */
 
     public ArduinoControllerSendCommand() {
         super("ArduinoControllerSendCommand");
@@ -12,11 +27,11 @@ public class ArduinoControllerSendCommand extends Command {
 
     @Override
     public void initialize() {
-        if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Red) {
-            Subsystems.arduino.sendCommand("0005356");
-        } else {
-            Subsystems.arduino.sendCommand("0005256");
-        }
+        // if (DriverStation.getInstance().getAlliance() == DriverStation.Alliance.Red) {
+        //     Subsystems.arduino.sendCommand("0005356");
+        // } else {
+        //     Subsystems.arduino.sendCommand("0005256");
+        // }
     }
 
     @Override
--- src/main/java/frc/robot/commands/DriveStraight.java
+++ src/main/java/frc/robot/commands/DriveStraight.java
@@ -1,62 +1,58 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.Subsystems;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import frc.robot.subsystems.Subsystems;
 
 public class DriveStraight extends Command {
 
-    private double ticks;
-    private boolean forward;
-    private double speed;
-
-    // private int covertToTicks(double inches)
+    // private double ticks;
+    // private boolean forward;
+    // private double speed;
 
     public DriveStraight(double inches, double Speed, boolean Forward, double Timeout) {
         super("DriveStraight");
-        requires(Subsystems.driveBase);
-        ticks = convertToTicks(inches);
-        forward = Forward;
-        speed = Speed;
+        // requires(Subsystems.driveBase);
+        // Set the class variables here like you would for your own constructor!
+        // Keep in mind that the number of inches should be positive.
+        setTimeout(Timeout);
     }
 
     @Override
     public void initialize() {
-        Subsystems.driveBase.zeroEncoderPosition();
-        Subsystems.driveBase.zeroGyroAngle();
-        SmartDashboard.putString("messy","EVERYTHING IS FINEE");
+        // Zero the encoder position, gyro, and any other instruments
     }
 
     @Override
     public void execute() {
-        double correction = Subsystems.driveBase.getGyroAngle();
-        correction *= 0.075;
-        correction += 1.0;
-        if (forward) {
-            Subsystems.driveBase.setMotors(-speed, -speed * correction);
-        } else {
-            Subsystems.driveBase.setMotors(speed * correction, speed);
-        }
+        // This will calculate the motor values needed to continue driving straight!
+        // double correction = Subsystems.driveBase.getGyroAngle();
+        // correction *= 0.075;
+        // correction += 1.0;
+        // if (forward) {
+        //     Subsystems.driveBase.setMotors(-speed, -speed * correction);
+        // } else {
+        //     Subsystems.driveBase.setMotors(speed * correction, speed);
+        // }
     }
 
     @Override
     public boolean isFinished() {
-        int leftPosition = Math.abs(Subsystems.driveBase.getLeftPosition());
-        int rightPosition = Math.abs(Subsystems.driveBase.getRightPosition());
-        return (leftPosition > ticks) || (rightPosition > ticks) || isTimedOut();
+        // If the robot has reached the length needed OR time has run out (with isTimedOut()), return false
+        return isTimedOut(); //placeholder for u
     }
 
     @Override
     public void interrupted() {
-        Subsystems.driveBase.setMotors(0,0);
+        // Stop the motors
     }
-
 
     @Override
     public void end() {
-        Subsystems.driveBase.setMotors(0,0);
+        // Stop the motors
     }
 
+    // Remember this?
     public double convertToTicks(double inches) {
         return (4096 / (6 * 3.1415926) * inches);
     }
